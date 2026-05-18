283475aba33c2ae7622e7b27e56c8296a27c8932
gCattt <78471254+gCattt@users.noreply.github.com>
2026-05-18 11:51:38 +0200

    feat(texture): :sparkles: improve random mask generation

--- src/texture/corrupt_textures.py
+++ src/texture/corrupt_textures.py
@@ -6,27 +6,44 @@ from PIL import Image, ImageDraw
 from src.utils.config_utils import load_yaml_config
 
 
-def generate_random_mask(width, height, num_shapes, thickness_min=4, thickness_max=30):
+def generate_random_mask(width, height, num_shapes, thickness_min, thickness_max, min_shape_size, max_ratio):
     mask = Image.new("L", (width, height), 0)
     draw = ImageDraw.Draw(mask)
 
     for _ in range(num_shapes):
         shape_type = random.choices(
             ["rectangle", "ellipse", "line"],
-            weights=[0.5, 0.4, 0.1]
+            weights=[0.3, 0.2, 0.5]
         )[0]
 
-        x1 = random.randint(0, width - 1)
-        y1 = random.randint(0, height - 1)
-        x2 = random.randint(x1, width)
-        y2 = random.randint(y1, height)
+        max_w = max(min_shape_size, int(width * max_ratio))
+        max_h = max(min_shape_size, int(height * max_ratio))
+
+        w_shape = random.randint(min_shape_size, max_w)
+        h_shape = random.randint(min_shape_size, max_h)
+
+        x1 = random.randint(0, max(0, width - w_shape))
+        y1 = random.randint(0, max(0, height - h_shape))
+        x2 = x1 + w_shape
+        y2 = y1 + h_shape
 
         if shape_type == "rectangle":
             draw.rectangle([x1, y1, x2, y2], fill=255)
         elif shape_type == "ellipse":
             draw.ellipse([x1, y1, x2, y2], fill=255)
         elif shape_type == "line":
+            x1 = random.randint(0, width - 1)
+            y1 = random.randint(0, height - 1)
+
+            max_len = int(min(width, height) * max_ratio)
+            dx = random.randint(-max_len, max_len)
+            dy = random.randint(-max_len, max_len)
+
+            x2 = np.clip(x1 + dx, 0, width - 1)
+            y2 = np.clip(y1 + dy, 0, height - 1)
+
             thickness = random.randint(thickness_min, thickness_max)
+
             draw.line([x1, y1, x2, y2], fill=255, width=thickness)
 
     return mask
@@ -40,11 +57,11 @@ def apply_corruption(texture, mask):
 
     return Image.fromarray(corrupted)
 
-def corrupt_single_texture(input_path, images_path, masks_path, num_shapes, thickness_min=4, thickness_max=30):
+def corrupt_single_texture(input_path, images_path, masks_path, num_shapes, thickness_min, thickness_max, min_shape_size, max_ratio):
     texture = Image.open(input_path).convert("RGB")
     w, h = texture.size
 
-    mask = generate_random_mask(w, h, num_shapes, thickness_min, thickness_max)
+    mask = generate_random_mask(w, h, num_shapes, thickness_min, thickness_max, min_shape_size, max_ratio)
     corrupted = apply_corruption(texture, mask)
 
     corrupted.save(images_path)
@@ -62,14 +79,16 @@ def corrupt_main():
     images_dir.mkdir(parents=True, exist_ok=True)
     masks_dir.mkdir(parents=True, exist_ok=True)
 
-    num_shapes = corrupt_cfg["num_shapes"]
-    thickness_min = corrupt_cfg["line_thickness_min"]
-    thickness_max = corrupt_cfg["line_thickness_max"]
-
     extensions = [
         ext.lower().lstrip(".")
         for ext in cfg.get("extensions", [])
     ]
+
+    num_shapes = corrupt_cfg["num_shapes"]
+    thickness_min = corrupt_cfg["line_thickness_min"]
+    thickness_max = corrupt_cfg["line_thickness_max"]
+    min_shape_size = corrupt_cfg["min_shape_size"]
+    max_ratio = corrupt_cfg["max_ratio"]
 
     files = []
     for ext in extensions:
@@ -82,7 +101,7 @@ def corrupt_main():
         images_path = images_dir / f"{name}_corrupted.png"
         masks_path = masks_dir / f"{name}_mask.png"
 
-        corrupt_single_texture(img_path, images_path, masks_path, num_shapes, thickness_min, thickness_max)
+        corrupt_single_texture(img_path, images_path, masks_path, num_shapes, thickness_min, thickness_max, min_shape_size, max_ratio)
 
 
 if __name__ == "__main__":
